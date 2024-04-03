d5ffd035cab05ce937d3bfbbc2a86699d1e2d784
jsong-kentech <jsong@kentech.ac.kr>
2024-04-03 10:35:00 +0900

    0403

--- repeat_DRT.m
+++ repeat_DRT.m
@@ -1,16 +1,13 @@
-% This code calculates a distribution of relaxation times (DRT) for a given
-% EIS data.
-
 clear; clc; close all
+
 %% Configuration
 
 % Data
-
     % Data path and name
-    %filename_data = "1. Li'SE'Li after 0.5 mAh plating GEIS.csv";
-    %filename_data = "1. Li'SE'Li after 0.5 mAh plating PEIS.csv";
-    filename_data = "2. SUS'SE'Li after 0.5 mAh plating PEIS.csv";
-    
+    filename_data = '3. Ag-In plating x10.csv';
+    % filename_data = '4. Ag plating x10.csv';
+
+
    % Freq range to include
     f_range = [-inf inf]; % [-inf inf] to include all data
 
@@ -39,10 +36,58 @@ clear; clc; close all
 
 %% Data Pre-processing
 
-    % Load the selected data
-    data = readmatrix(filename_data); % 숫자만 읽어 옴. expecting [freq (Hz), Z_real (Ohm), Z_ohm]
+% load raw data
+data = readmatrix(filename_data);
 
-    % 지정된 주파수 범위 외 삭제
+
+% data processing
+    % delete NAN rows
+    data = data(~isnan(data(:,1)),:);
+
+    % assign set number
+    set_now = 1;
+    for i = 1:length(data(:,1))-1
+        
+        data(i,4) = set_now;
+        
+        if data(i,1)-data(i+1,1) < 0
+            
+            set_now = set_now +1;
+        end
+
+        if i == length(data(:,1))-1
+            data(i+1,4) = set_now;
+
+        end
+
+
+    end
+
+    % separate by set numbers
+    set_vec = unique(data(:,4));
+%    set_vec = [1 4 7 10];
+    dstruct = struct('eis',cell(length(set_vec),1));
+    for j = 1:length(set_vec)
+        dstruct(j).eis = data(data(:,4)==set_vec(j),:);
+    end
+
+clear data
+
+%% Run DRT
+
+
+set_vec = [1 4 7 10];
+
+c_mat = cool(length(set_vec));
+
+
+for k = 1:length(set_vec)
+
+   % run_DRT.m 240331 line 45
+    j = set_vec(k);
+    data = dstruct(j).eis;
+
+   % 지정된 주파수 범위 외 삭제
     data = data(data(:,1) > f_range(1),:);
     data = data(data(:,1) < f_range(2),:);
 
@@ -53,23 +98,19 @@ clear; clc; close all
     f_data = data(:,1); % [Hz] frequency data (vector)
     z_data = data(:,2) -data(:,3)*1i; % [Ohm] impedance data (vector, complex)
     
-
-
     % plot the imported impedance data
     figure(1);
     subplot(1,3,1)
     hold on;
-    plot(real(z_data),-imag(z_data),'ko')
+    plot(real(z_data),-imag(z_data),'o','Color',c_mat(k,:))
     set(gca,'Box','on',... %Axis Properties: BOX   
     'PlotBoxAspectRatio',[1 1 1],... % Size - you can either use 'position' or 'dataaspectratio' or their combinations
     'FontUnits','points','FontSize',10,'FontName','Times New Roman',... % Fonts
     'XLim',[0.8*min(real(z_data)), 1.2*max(real(z_data))],'Ylim',[0,1.2*max(real(z_data))-0.8*min(real(z_data))])
     xlabel('Z_{re} [Ohm]')
     ylabel('-Z_{im} [Ohm]')
-    legend_fig1{1} = 'exp data';
+    legend_fig1{2*(k-1)+1} = sprintf('exp data, cyc=%d', j);
     legend(legend_fig1,'location','northwest')
-
-
 
 %% Preparing the Inversion
 
@@ -88,9 +129,6 @@ clear; clc; close all
         % Kernel
         [K,~,D] = DRT_Kernel(type_intercept,type_kernel,x,ppd_t,t,zeros(M,1));
 
-
-
-
 %% DRT Inversion: (3) Inversion with the optimal hyperparameters
 
     % Calculate weighting matrix
@@ -105,18 +143,18 @@ clear; clc; close all
 
     % Plot the solution
         figure(1); subplot(1,3,1); hold on;
-        plot(real(z_D_star),-imag(z_D_star),'-b','linewidth',2);
-        legend_fig1{end+1} = 'DRT model';
+        plot(real(z_D_star),-imag(z_D_star),'-','linewidth',2,'Color',c_mat(k,:));
+        legend_fig1{2*(k-1)+2} = sprintf('DRT model, cyc=%d', j);
         legend(legend_fig1)
 
         figure(1); subplot(1,3,2); hold on;
-        plot(log10(tau),q_star(2:end),'-')
+        plot(log10(tau),q_star(2:end),'-','Color',c_mat(k,:))
         % axis([log10(exp(t_lb)),log10(exp(t_ub)),0,1.1*max(q_star(2:end-1))]);
         axis([log10(exp(t_lb)),log10(exp(t_ub)),0,2.2]);
         xlabel ('t = log_{10}(\tau)')
         ylabel ('q = \tauP(\tau)')
-        legend_fig4{1} = 'DRT solution';
-        legend(legend_fig4)
+        legend_fig2{k} = sprintf('DRT, cyc = %d',j);
+        legend(legend_fig2)
 
     set(gca,'Box','on',... %Axis Properties: BOX   
     'PlotBoxAspectRatio',[1 1 1],... % Size - you can either use 'position' or 'dataaspectratio' or their combinations
@@ -124,4 +162,25 @@ clear; clc; close all
 
     set(gcf,'position',[100 600 900 400])
 
-% end.
+
+    % 
+    dstruct(j).drt = [log10(tau'),tau',q_star(2:end)];%
+
+
+
+
+end
+
+return
+%% Output in excel
+
+[~,filename_noext,~] = fileparts(filename_data);
+
+for j = 1:length(set_vec)
+
+    sheetname = sprintf('cyc %d ',j);
+    writematrix(dstruct(j).eis,[filename_noext '.xlsx'],'sheet',[sheetname 'EIS'])
+    writematrix(dstruct(j).drt,[filename_noext '.xlsx'],'sheet',[sheetname 'DRT'])
+
+
+end
